8f1bf79de37302a4a54ff1c74fad4f9260ae57d2
Mara Schulze <marschul@2-H-5.42heilbronn.de>
2024-04-17 20:15:38 +0200

    Updated hpp file

--- includes/IrcApplicationLayer.hpp
+++ includes/IrcApplicationLayer.hpp
@@ -6,7 +6,7 @@
 /*   By: marschul <marschul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:39:00 by marschul          #+#    #+#             */
-/*   Updated: 2024/04/17 18:30:57 by marschul         ###   ########.fr       */
+/*   Updated: 2024/04/17 20:14:12 by marschul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ class  IrcApplicationLayer {
 	public:
 		IrcApplicationLayer();
 		~IrcApplicationLayer();
-		void	connect(struct sockaddr_in address);
+		int		connect(struct sockaddr_in address);	// returns fd of pipe with data to the client,
+			// I need the host information (can just be the IP address) and an identifier for the client, see receive method
 		void	disconnect(struct sockaddr_in address);
-		void	receive(struct sockaddr_in address, std::string line);
-		int		getPipeFd();
+		void	receive(struct sockaddr_in address, std::string line);	// here I need some identification to identify the client,
+			// but you can choose the first parameter, if it is the address or the socket fd, how you like it best.
 };
 
 #endif
